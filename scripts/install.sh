9e84e58c058ecd48476a8a645b3a279d05477d09
Your GitHub Username <youremail@example.com>
2023-08-22 19:14:37 +0530

    fix: Added fix for release artifacts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cp tibco-ems.jar /var/db/newrelic-infra/custom-integrations
+cp softwareag-ums.jar /var/db/newrelic-infra/custom-integrations
 
-cp tibco-ems-definition.yml /var/db/newrelic-infra/custom-integrations/
+cp softwareag-ums-definition.yml /var/db/newrelic-infra/custom-integrations/
 
-cp tibco-ems-config.yml /etc/newrelic-infra/integrations.d/
+cp softwareag-ums-config.yml /etc/newrelic-infra/integrations.d/
 
-cp tibco-ems-server-config.json /etc/newrelic-infra/integrations.d/
+cp softwareag-ums-server-config.json /etc/newrelic-infra/integrations.d/
