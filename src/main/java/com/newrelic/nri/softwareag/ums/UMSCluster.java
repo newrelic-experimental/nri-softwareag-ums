694033ee39d74744d73c905940705a64d41693fd
gsidhwani_nr <gsidhwani@newrelic.com>
2023-11-09 22:20:27 +0530

    updated header

--- src/main/java/com/newrelic/nri/softwareag/ums/UMSCluster.java
+++ src/main/java/com/newrelic/nri/softwareag/ums/UMSCluster.java
@@ -1,13 +1,5 @@
 package com.newrelic.nri.softwareag.ums;
 
-/*
-*
-*   Copyright (c) 1999 - 2011 my-Channels Ltd
-*   Copyright (c) 2012 - 2022 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates and/or their licensors.
-*
-*   Use, reproduction, transfer, publication or disclosure is prohibited except as specifically provided for in your License Agreement with Software AG.
-*
-*/
 
 import com.pcbsys.nirvana.client.nChannel; 
 import com.pcbsys.nirvana.client.nChannelAttributes;
